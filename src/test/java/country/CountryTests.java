971ca89888521fb54ea766fbe31bf6c6a46b1ac2
Le Tien Dat <letiendat22071999@gmail.com>
2025-06-05 21:54:23 +0700

    [API-AUTO-04] Verify Country With Pagination

--- src/test/java/country/CountryTests.java
+++ src/test/java/country/CountryTests.java
@@ -7,9 +7,13 @@ import io.restassured.RestAssured;
 import io.restassured.common.mapper.TypeRef;
 import io.restassured.response.Response;
 import model.Country;
+import model.CountryPagination;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
@@ -24,6 +28,7 @@ import static org.hamcrest.Matchers.*;
 
 public class CountryTests {
     private static final String GET_COUNTRIES_API = "/api/v1/countries";
+    private static final String GET_COUNTRY_PAGINATION_API = "/api/v4/countries";
     private static final String GET_COUNTRY_API = "/api/v1/countries/{code}";
     private static final String X_POWERED_BY_HEADER = "X-Powered-By";
     private static final String X_POWERED_BY_HEADER_VALUE = "Express";
@@ -32,6 +37,8 @@ public class CountryTests {
     private static final String GET_COUNTRY_WITH_FILTER_API = "/api/v3/countries";
     private static final String GDP_FILTER = "gdp";
     private static final String OPERATOR_FILTER = "operator";
+    private static final String PAGE = "page";
+    private static final String SIZE = "size";
 
     @BeforeAll
     static void setup() {
@@ -112,7 +119,7 @@ public class CountryTests {
     void verifySchemaOfGetCountryApiWithFilter() {
         RestAssured.given().log().all()
                 .queryParam(GDP_FILTER, 5000)
-                .queryParam( OPERATOR_FILTER, ">")
+                .queryParam(OPERATOR_FILTER, ">")
                 .get(GET_COUNTRY_WITH_FILTER_API)
                 .then()
                 .log().all()
@@ -125,7 +132,134 @@ public class CountryTests {
     void verifyGetCountryApiWithFilterGreaterThan() {
         Response response = RestAssured.given().log().all()
                 .queryParam(GDP_FILTER, 5000)
-                .queryParam( OPERATOR_FILTER, ">")
+                .queryParam(OPERATOR_FILTER, ">")
+                .get(GET_COUNTRY_WITH_FILTER_API);
+
+        // 1. Verify status code
+        response.then().log().all().statusCode(200);
+
+        // 2. Verify headers
+        response.then().header(X_POWERED_BY_HEADER, equalTo(X_POWERED_BY_HEADER_VALUE))
+                .header(CONTENT_TYPE_HEDAER, equalTo(CONTENT_TYPE_HEDAER_VALUE));
+
+        // 3. Verify body
+        List<Country> actual = response.body().as(new TypeRef<>() {
+        });
+
+        for (Country country : actual) {
+            assertThat(country.getGdp(), greaterThan(5000f));
+        }
+    }
+
+    static Stream<Arguments> getCountryWithFilterProvider() {
+        return Stream.of(
+                Arguments.of(">", 5000, greaterThan(5000f)),
+                Arguments.of(">=", 5000, greaterThanOrEqualTo(5000f)),
+                Arguments.of("<", 5000, lessThan(5000f)),
+                Arguments.of("<=", 5000, lessThanOrEqualTo(5000f)),
+                Arguments.of("==", 5000, equalTo(5000f))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getCountryWithFilterProvider")
+    void verifyGetCountryApiWithFilter(String operator, int gdp, Matcher expected) {
+        Response response = RestAssured.given().log().all()
+                .queryParam(GDP_FILTER, gdp)
+                .queryParam(OPERATOR_FILTER, operator)
+                .get(GET_COUNTRY_WITH_FILTER_API);
+
+        // 1. Verify status code
+        response.then().log().all().statusCode(200);
+
+        // 2. Verify headers
+        response.then().header(X_POWERED_BY_HEADER, equalTo(X_POWERED_BY_HEADER_VALUE))
+                .header(CONTENT_TYPE_HEDAER, equalTo(CONTENT_TYPE_HEDAER_VALUE));
+
+        // 3. Verify body
+        List<Country> actual = response.body().as(new TypeRef<>() {
+        });
+
+        for (Country country : actual) {
+            assertThat(country.getGdp(), expected);
+        }
+    }
+
+    @Test
+    void verifySchemaOfGetCountryApiWithPagination() {
+        RestAssured.given().log().all()
+                .queryParam(PAGE, 1)
+                .queryParam(SIZE, 4)
+                .get(GET_COUNTRY_PAGINATION_API)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("json-schema/country-pagination-schema.json"));
+    }
+
+    @Test
+    void verifyCountryApiDataWithPagination() throws JsonProcessingException {
+        int testSize = 4;
+
+        Response response = getCountryApiWithPagination(testSize, 1);
+        // 1. Verify status code
+        response.then().log().all().statusCode(200);
+
+        // 2. Verify headers
+        response.then().header(X_POWERED_BY_HEADER, equalTo(X_POWERED_BY_HEADER_VALUE))
+                .header(CONTENT_TYPE_HEDAER, equalTo(CONTENT_TYPE_HEDAER_VALUE));
+
+        // 3. Verify body
+        CountryPagination actualDataFirstPage = response.body().as(CountryPagination.class);
+        verifyPage(actualDataFirstPage, testSize, 1, testSize);
+
+        // 4. get second page
+        response = getCountryApiWithPagination(testSize, 2);
+
+        response.then().log().all().statusCode(200);
+        CountryPagination actualDataSecondPage = response.body().as(CountryPagination.class);
+        verifyPage(actualDataSecondPage, testSize, 2, testSize);
+
+        // 5. Verify data from first page vs second page
+        assertThat(actualDataFirstPage.getData().containsAll(actualDataSecondPage.getData()), equalTo(false));
+        assertThat(actualDataSecondPage.getData().containsAll(actualDataFirstPage.getData()), equalTo(false));
+
+        // 6. Verify last page
+        int lastPage = actualDataSecondPage.getTotal() / testSize;
+        int sizeOfLastPage = actualDataSecondPage.getTotal() % testSize;
+        if (sizeOfLastPage != 0) {
+            lastPage++;
+        } else {
+            sizeOfLastPage = testSize;
+        }
+
+        response = getCountryApiWithPagination(testSize, lastPage);
+        response.then().log().all().statusCode(200);
+        CountryPagination actualDataLastPage = response.body().as(CountryPagination.class);
+        verifyPage(actualDataLastPage, testSize, lastPage, sizeOfLastPage);
+    }
+
+    private static void verifyPage(CountryPagination pageData, int expectedSize, int expectedPage, int expectedLength) {
+        assertThat(pageData.getPage(), equalTo(expectedPage));
+        assertThat(pageData.getSize(), equalTo(expectedSize));
+        assertThat(pageData.getData(), hasSize(expectedLength));
+    }
+
+    private static Response getCountryApiWithPagination(int testSize, int page) {
+        Response response = RestAssured.given().log().all()
+                .queryParam(PAGE, page)
+                .queryParam(SIZE, testSize)
+                .get(GET_COUNTRY_PAGINATION_API);
+        return response;
+    }
+
+    /*
+    @Test
+    void verifyGetCountryApiWithFilterLessThan() {
+        Response response = RestAssured.given().log().all()
+                .queryParam(GDP_FILTER, 5000)
+                .queryParam( OPERATOR_FILTER, "<")
                 .get(GET_COUNTRY_WITH_FILTER_API);
 
         // 1. Verify status code
@@ -140,7 +274,54 @@ public class CountryTests {
         });
 
         for (Country country : actual){
-            assertThat(country.getGdp(), greaterThan(5000f));
+            assertThat(country.getGdp(), lessThan(5000f));
         }
     }
+
+    @Test
+    void verifyGetCountryApiWithFilterLessThanOrEqual() {
+        Response response = RestAssured.given().log().all()
+                .queryParam(GDP_FILTER, 5000)
+                .queryParam( OPERATOR_FILTER, "<=")
+                .get(GET_COUNTRY_WITH_FILTER_API);
+
+        // 1. Verify status code
+        response.then().log().all().statusCode(200);
+
+        // 2. Verify headers
+        response.then().header(X_POWERED_BY_HEADER, equalTo(X_POWERED_BY_HEADER_VALUE))
+                .header(CONTENT_TYPE_HEDAER, equalTo(CONTENT_TYPE_HEDAER_VALUE));
+
+        // 3. Verify body
+        List<Country> actual = response.body().as(new TypeRef<>() {
+        });
+
+        for (Country country : actual){
+            assertThat(country.getGdp(), lessThanOrEqualTo(5000f));
+        }
+    }
+
+    @Test
+    void verifyGetCountryApiWithFilterEqual() {
+        Response response = RestAssured.given().log().all()
+                .queryParam(GDP_FILTER, 5000)
+                .queryParam( OPERATOR_FILTER, "==")
+                .get(GET_COUNTRY_WITH_FILTER_API);
+
+        // 1. Verify status code
+        response.then().log().all().statusCode(200);
+
+        // 2. Verify headers
+        response.then().header(X_POWERED_BY_HEADER, equalTo(X_POWERED_BY_HEADER_VALUE))
+                .header(CONTENT_TYPE_HEDAER, equalTo(CONTENT_TYPE_HEDAER_VALUE));
+
+        // 3. Verify body
+        List<Country> actual = response.body().as(new TypeRef<>() {
+        });
+
+        for (Country country : actual){
+            assertThat(country.getGdp(), equalTo(5000f));
+        }
+    }
+ */
 }
