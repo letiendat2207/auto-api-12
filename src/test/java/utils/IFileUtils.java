34514ebc04b97143e4fa90f2eedc04d9775907f3
Le Tien Dat <letiendat22071999@gmail.com>
2025-06-30 15:59:01 +0700

    [API-AUTO-19] graphql query test

---
+++ src/test/java/utils/IFileUtils.java
@@ -0,0 +1,14 @@
+package utils;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class IFileUtils {
+    public static String readFileFromResources(String path) throws IOException {
+        ClassLoader classLoader = IFileUtils.class.getClassLoader();
+        File file = new File(classLoader.getResource(path).getFile());
+        return FileUtils.readFileToString(file, "UTF-8");
+    }
+}
